b7323b9cc1a29d34f447ee743224d1d2398e7c36
Carlos Perez <solracias@gmail.com>
2017-04-09 10:39:56 -0700

    randome number generator is working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,39 @@
 /*notes
-- There will be four crystals displayed as buttons on the page.
+- There will be four crystals displayed as buttons on 
+	the page.
 -- dynamically load the images
--- Each crystal should have a random hidden value between 1 - 12.
--- The player will have to guess the answer by clicking on each image
+-- Each crystal should have a random hidden value 
+	between 1 - 12.
+-- The player will have to guess the answer by 
+	clicking on each image
 	if player clicks on image, the counter goes up.
--- When the player clicks on a crystal, it will add a specific amount of 
+-- When the player clicks on a crystal, it will 
+	add a specific amount of 
 	points to the player's total score.
-	--- Your game will hide this amount until the player clicks a crystal.
-	--- When they do click one, update the player's score counter.
+	--- Your game will hide this amount until 
+		the player clicks a crystal.
+	--- When they do click one, update the 
+		player's score counter.
 ==============================*/
 $(document).ready(function(){
 
 
+	var lowEnd = 19;
+	var highEnd = 120;
+	var arr = [];
+	while (lowEnd <= highEnd) {
+		arr.push(lowEnd++);
+	}
+	randomNumberChallenge = arr[Math.floor(Math.random()*arr.length)];
+	console.log(randomNumberChallenge);
+
+	$("#randomNumber").html(randomNumberChallenge);
+});
+
+
+// $("#blueGem").on("click", function(){
+
+// })
 
 
 
@@ -34,9 +56,11 @@ $(document).ready(function(){
 
 
 /*
-- The player will be shown a random number at the start of the game.
+- The player will be shown a random number 
+at the start of the game.
 -- use jquery to initialize this
--- The random number shown at the start of the game should be between 19 - 120.
+-- The random number shown at the 
+start of the game should be between 19 - 120.
 ===============================
 
 - The player wins 
