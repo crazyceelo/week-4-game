4582d1ff17f9243b68ea220956fc9328238d7bdf
Carlos Perez <solracias@gmail.com>
2017-04-09 20:22:29 -0700

    done! I removed scope, and put the if statements in the click function. Now all the code works, but itis not neat

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,10 +17,9 @@
 ==============================*/
 $(document).ready(function(){
 
-// Global Variables
+// Initial Variables
 var wins = 0;
 var losses = 0;
-var totalScore = 0;
 var gemLowEnd = 1;
 var gemHighEnd = 12;
 var gemArr = [];
@@ -31,65 +30,90 @@ var pinkGem = 0;
 var lowEnd = 19;
 var highEnd = 120;
 var arr = [];
-
-	function startGame(){
-
-		while (gemLowEnd <= gemHighEnd) {
-			gemArr.push(gemLowEnd++);
-		}
-		randomGemValue1 = gemArr[Math.floor(Math.random()*gemArr.length)];
-		randomGemValue2 = gemArr[Math.floor(Math.random()*gemArr.length)];
-		randomGemValue3 = gemArr[Math.floor(Math.random()*gemArr.length)];
-		randomGemValue4 = gemArr[Math.floor(Math.random()*gemArr.length)];
-		blueGem = randomGemValue1;
-		greenGem = randomGemValue2;
-		orangeGem = randomGemValue3;
-		pinkGem = randomGemValue4;
-		$("#blueGem").attr("value", blueGem);
-		$("#greenGem").attr("value", greenGem);
-		$("#orangeGem").attr("value", orangeGem);
-		$("#pinkGem").attr("value", pinkGem);
-		console.log(blueGem);
-		console.log(greenGem);
-		console.log(orangeGem);
-		console.log(pinkGem);
+var totalScore = 0;
+var randomNumber = 0;
 
 
-     // for (var i = 0; i < letters.length; i++){
-     //    var letterBtn = $("<button>");
-     //    letterBtn.addClass("letter-button letter letter-button-color");
-     //    letterBtn.attr("data-letter", letters[i]);
-     //    letterBtn.html(letters[i]);
-     //    $("#buttons").append(letterBtn);
+// this resets the game.
+function resetGame(){
+	var randomGemValue1 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	var randomGemValue2 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	var randomGemValue3 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	var randomGemValue4 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	blueGem = randomGemValue1;
+	greenGem = randomGemValue2;
+	orangeGem = randomGemValue3;
+	pinkGem = randomGemValue4;
+	$("#blueGem").attr("value", blueGem);
+	$("#greenGem").attr("value", greenGem);
+	$("#orangeGem").attr("value", orangeGem);
+	$("#pinkGem").attr("value", pinkGem);
+
+	while (lowEnd <= highEnd) {
+		arr.push(lowEnd++);
+	}
+	randomNumberChallenge = arr[Math.floor(Math.random()*arr.length)];
+	randomNumber = randomNumberChallenge;
+	$("#randomNumber").html(randomNumber);
+	totalScore = 0;
+	$("#totalScore").html(totalScore);
+}
+
+//initial random variables
+	while (gemLowEnd <= gemHighEnd) {
+		gemArr.push(gemLowEnd++);
+	}
+	var randomGemValue1 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	var randomGemValue2 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	var randomGemValue3 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	var randomGemValue4 = gemArr[Math.floor(Math.random()*gemArr.length)];
+	blueGem = randomGemValue1;
+	greenGem = randomGemValue2;
+	orangeGem = randomGemValue3;
+	pinkGem = randomGemValue4;
+	$("#blueGem").attr("value", blueGem);
+	$("#greenGem").attr("value", greenGem);
+	$("#orangeGem").attr("value", orangeGem);
+	$("#pinkGem").attr("value", pinkGem);
+
+	while (lowEnd <= highEnd) {
+		arr.push(lowEnd++);
+	}
+	randomNumberChallenge = arr[Math.floor(Math.random()*arr.length)];
+	randomNumber = randomNumberChallenge;
+	$("#randomNumber").html(randomNumber);
 
 
-		while (lowEnd <= highEnd) {
-			arr.push(lowEnd++);
-		}
-		randomNumberChallenge = arr[Math.floor(Math.random()*arr.length)];
-		$("#randomNumber").html(randomNumberChallenge);
+	$("#wins").html(wins);
+	$("#losses").html(losses);
+	$("#totalScore").html(totalScore);
 
 
-		$("#wins").html(wins);
-		$("#losses").html(losses);
+// onclick function
+		$(".allGems").on("click", function(){
+		var gemBtns = ($(this).attr("value"));
+		gemBtns = parseInt(gemBtns);
+		totalScore += gemBtns;
 		$("#totalScore").html(totalScore);
-	}
-	
-
-	function addTotalScore(){
-			$("#blueGem").on("click", function(){
-			var blueBtn = ($(this).attr("value"));
-			blueBtn = parseInt(blueBtn);
-			totalScore += blueBtn;
+		
+		if (randomNumber === totalScore) {
+			wins++;
+			$("#wins").html(wins);
 			$("#totalScore").html(totalScore);
-			console.log(totalScore);
-		})
-	}
+			alert("you win");
+			resetGame();
+		}
+		else if (totalScore > randomNumber) {
+			losses++;
+			$("#losses").html(losses);
+			$("#totalScore").html(totalScore);
+			alert("you lose");
+			resetGame();
 
+		}
+	})
+})
 
-	startGame();
-	addTotalScore();
-});
 
 
 
